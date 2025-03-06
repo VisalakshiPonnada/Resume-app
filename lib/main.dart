9da3e20eb38fd07e0a3371e412b181413d0174d5
Visalakshi <ponnadavisalakshi@gmail.com>
2025-03-06 20:59:13 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:resume/util/app_theme.dart';
+import 'package:resume/view/main_view.dart';
+void main(){
+  runApp(resume());
+}
+class resume extends StatelessWidget {
+  const resume({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+    title: "resume app" ,
+    theme: AppTheme.lightTheme,
+    darkTheme: AppTheme.DarkTheme,
+    themeMode: ThemeMode.system,
+    home:MainView(),
+    );
+    
+  }
+}
